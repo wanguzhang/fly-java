3d152e9b6b8b6428edf835818aa7eeec12665a80
张攀钦 <zhangpanqin>
2019-06-10 18:12:38 +0800

    feat:初始化项目

---
+++ src/main/java/com/fly/study/FlyJavaApplication.java
@@ -0,0 +1,19 @@
+package com.fly.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * @author 张攀钦
+ * @date 2019/6/10-18:06
+ * 项目启动
+ */
+@SpringBootApplication(exclude =DataSourceAutoConfiguration.class )
+public class FlyJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyJavaApplication.class, args);
+    }
+
+}
