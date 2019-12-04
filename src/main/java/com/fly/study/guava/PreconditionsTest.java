aecb7512e3d56be91beadff4631abb52c4cff6d8
张攀钦 <zhangpanqin>
2019-12-04 22:46:34 +0800

    feat:guava 添加

---
+++ src/main/java/com/fly/study/guava/PreconditionsTest.java
@@ -0,0 +1,17 @@
+package com.fly.study.guava;
+
+import org.junit.Test;
+
+/**
+ * @author 张攀钦
+ * @date 2019-12-04-22:32
+ * @description Preconditions 学习
+ */
+public class PreconditionsTest {
+
+    @Test
+    public void run1() {
+
+    }
+
+}
