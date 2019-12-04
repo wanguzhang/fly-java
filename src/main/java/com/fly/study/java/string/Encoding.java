03ae8bee4843cc71840734c3990f36c1e40a754b
张攀钦 <zhangpanqin>
2019-12-04 15:56:17 +0800

    feat:查看 ArrayList 和 CopyOnWriteArrayList 的源码

---
+++ src/main/java/com/fly/study/java/string/Encoding.java
@@ -0,0 +1,17 @@
+package com.fly.study.java.string;
+
+import org.junit.Test;
+
+/**
+ * @author 张攀钦
+ * @date 2019-12-04-9:27
+ * @description
+ */
+public class Encoding {
+
+    @Test
+    public void run11() {
+        System.out.println(System.getProperty("file.encoding"));
+    }
+
+}
